8963465670f6c3719eebf784633b8e23a9f51199
tino <tino.imbrogno@insa-rennes.fr>
2026-07-29 22:13:23 +0200

    fix

--- app/api/vdocipher/[vdoCipher_id]/route.ts
+++ app/api/vdocipher/[vdoCipher_id]/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 import { createClient } from "@/lib/supabase/server";
+import { json } from "stream/consumers";
 
 export async function GET(
     request: Request,
@@ -48,6 +49,14 @@ export async function GET(
     // appel VdoCipher
     //------------------------------------
 
+    const key = process.env.VDOCIPHER_API_SECRET;
+
+    console.log({
+        exists: !!key,
+        length: key?.length,
+        start: key?.slice(0, 5),
+    });
+
     const response = await fetch(
         "https://www.vdocipher.com/api/videos/" +
         vdoCipher_id +
@@ -62,6 +71,7 @@ export async function GET(
             },
             body: JSON.stringify({
                 ttl: 300,
+                json: true,
 
                 annotate: JSON.stringify({
                     email: user.email,
